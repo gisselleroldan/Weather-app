3b13669336ddfcded4b5694c06d3404082a790ce
Gisselle <g.roldan527@gmail.com>
2021-05-10 11:25:43 -0700

    added files

---
+++ scripts/api.js
@@ -0,0 +1,1 @@
+let apiKey = "9361bd03ec7175e9eea92fdf7a717518";
---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+input = prompt("Enter a city name");
+
+// Weather API
+let result = fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    console.log(data.main.temp);
+
+    let kelvin = data.main.temp;
+    let degC = Math.floor(kelvin - 273.15);
+    console.log(degC);
+    let degF = Math.floor(degC * 1.8 + 32);
+    console.log(degF);
+
+    let p = document.querySelector("p");
+    p.textContent = `Current Temp in ${input} is ${degF.toString()}`;
+  });
